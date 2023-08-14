52d23cbb1850375a62c98f7606d0757239c9322a
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-08-14 14:36:29 -0400

    set up react with flask template

---
+++ server/seed.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Standard library imports
+from random import randint, choice as rc
+
+# Remote library imports
+from faker import Faker
+
+# Local imports
+from app import app
+from models import db, User, HauntedLocation, Visit, Experience
+
+if __name__ == '__main__':
+    fake = Faker()
+    with app.app_context():
+        pass
+        # print('Clearing database...')
+        # User.query.delete()
+        # HauntedLocation.query.delete()
+        # Visit.query.delete()
+        # Experience.query.delete()
