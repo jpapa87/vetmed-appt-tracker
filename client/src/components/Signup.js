64ee0b33619de44c6c881be7b868d88647a163cd
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-08-15 22:24:00 -0400

    building models

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,64 +3,19 @@ import { useHistory } from "react-router-dom";
 // Not currently needed, stretch goal:
 
 
-function SignUp({setUser}) {
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [email, setEmail] = useState("")
-    const [age, setAge] = useState("")
-    const [errorMessage , setErrorMessage] = useState ("")
-    
-
-    let history = useHistory()
-
-    function handleUsernameChange(e) {
-        setUsername(e.target.value)
-    }
-
-    function handlePasswordChange(e) {
-        setPassword(e.target.value)
-    }
-
-    function handleEmailChange(e) {
-        setEmail(e.target.value)
-    }
-
-    function handleAgeChange(e) {
-        setAge(e.target.value)
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault()
-        const newUser = {
-            username: username,
-            password: password,
-            email: email,
-            age: age
-        }
-        console.log(newUser)
-        fetch('/users', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(newUser)
-        })
-        // TODO Fix everything here with .then(1st) and .then(2nd)
-        // FIXME Mainly the issue is that the user is currently just console logged and errors are not handled
-        .then(r => {
-            if (r.ok) {
-                r.json().then((newUser) => {
-                    setUser(newUser)
-                    history.push('/')
-                })
+    function SignUp(){
+        const [username, setUsername] = useState('')
+        const [email, setEmail] = useState('')
+        const [password, setPassword] = useState('')
+        
+        function handleSubmit(e) {
+            e.preventDefault()
+            const newUser = {
+                username: username,
+                email: email,
+                password: password
             }
-            else {
-                e.preventDefault()
-                setErrorMessage("User not created. Username must be 3 characters or more, email must include @, and you must be 18 or older to sign up.")
-            }
-        })
-        e.target.reset()
     }
-
-    
     return (
         <>
             <form 
@@ -74,13 +29,6 @@ function SignUp({setUser}) {
                     placeholder="Create Username"
                     onChange={handleUsernameChange}
                 />
-                <input className="create-password"
-                    type="password"
-                    name="password"
-                    value={password}
-                    placeholder="Create Password"
-                    onChange={handlePasswordChange}
-                />
                 <input className="enter-email"
                     type="text"
                     name="email"
@@ -88,12 +36,12 @@ function SignUp({setUser}) {
                     placeholder="Enter Email"
                     onChange={handleEmailChange}
                 />
-                <input className="enter-age"
-                    type="text"
-                    name="age"
-                    value={age}
-                    placeholder="Enter Age"
-                    onChange={handleAgeChange}
+                <input className="create-password"
+                    type="password"
+                    name="password"
+                    value={password}
+                    placeholder="Create Password"
+                    onChange={handlePasswordChange}
                 />
                 <input className="create-new-user"
                     type="submit"
@@ -101,9 +49,9 @@ function SignUp({setUser}) {
                     value="Create New User"
                 />
             </form>
-            <div>
+            {/* <div>
                 {errorMessage}
-            </div>
+            </div> */}
         </>
     )
 }
--- server/models.py
+++ server/models.py
@@ -5,3 +5,69 @@ from sqlalchemy.orm import validates
 
 
 from config import db, bcrypt
+
+
+
+
+class Vet(db.Model , SerializerMixin):
+    __tablename__ = 'vets'
+
+    id = db. Column(db.Integer, primary_key= True)
+    name = db.Column(db.String)
+    specialty = db.Column(db.String)
+
+
+
+    soaps = db.relationship('Soap' , back_populates= 'vets', cascade='all, delete-orphan')
+    patients = association_proxy('soaps' , 'patients')
+
+
+    # _password_hash = db.Column(db.String)
+
+    # serialize_rules = ('-_password_hash')
+
+    # @property
+    # def password_hash(self):
+    #     return self._password_hash
+    
+    # @password_hash.setter
+    # def password_hash(self, new_password):
+    #     # self._password_hash = new_password
+    #     #encryption
+    #     byte_object = new_password.encode('utf-8')
+    #     encrypted_hash_object = bcrypt.generate_password_hash(byte_object)
+    #     hash_object = encrypted_hash_object.decode('utf-8')
+    #     self._password_hash = hash_object
+    
+    # def authenticate(self, password):
+    #     return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+
+class Soap(db.Model, SerializerMixin):
+    __tablename__ = 'soaps'
+
+    id = db.Column(db.Integer , primary_key= True)
+    ailment = db.Column(db.String)
+    body = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    vet_id = db.Column(db.Integer , db.ForeignKey('vets.id'))
+    patient_id = db.Column(db.Integer , db.ForeignKey('patient.id'))
+
+
+    vet = db.relationship('Vet', back_populates='soaps')
+    patient = db.relationship('Patient', back_populates="soaps")
+
+
+class Patient(db.Model , SerializerMixin):
+    __tablename__ = 'patients'
+
+    id = db.Column(db.Integer , primary_key= True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    species = db.Column(db.String)
+
+    vet = db.relationship('Vet' , back_populates= 'patients')
+    soap = db.relationship('Soap' , back_populates= 'patients')
+
+    # TODO serialize_rules = ('-', '-')
