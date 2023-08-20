8d48ee02c832029e37bb60d61e218e5db4341042
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-08-20 15:50:03 -0400

    CSS on homepage and some on login and signup

--- client/src/components/SoapsCard.js
+++ client/src/components/SoapsCard.js
@@ -3,12 +3,12 @@ import { useState } from "react";
 
 function SoapsCard({ ailment, body, created_at, vet_id, patient_id}) {
     return (
-    <div className="soap-card" style={{width: "18rem"}}>
+    <div className="card" style={{width: "18rem"}}>
         <h4>Ailment: {ailment}</h4>
         <h4>Vet ID: {vet_id}</h4>
         <h4>Patient ID: {patient_id}</h4>
         <h4>Created At: {created_at}</h4>
-        <h4>Enter SOAP notes here: {body}</h4>
+        <h4>Assesment: {body}</h4>
     </div>
     );
 }
